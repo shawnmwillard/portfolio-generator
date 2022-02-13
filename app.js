0a2282814bfc17e1b15657c64e5985d76f2b380a
Shawn Willard <shawnmwillard@gmail.com>
2022-02-12 20:35:42 -0600

    made current with Module 9.2 source code

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
 
 const [name, github] = profileDataArgs;
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if(err) throw err;
+console.log(name, github);
 
-  console.log('Portfolio complete! Checkout index.html to see the output!');
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
 });
